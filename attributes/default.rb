3a523abf651c29f37e21e8b60b2e6845ed268b28
Mahmoud Ismail <mahmoudahmedismail@gmail.com>
2015-09-18 08:56:37 +0200

    use a different name for apache hopsbench

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 include_attribute "hadoop"
 
-default[:hopsbench][:jar_url] = "http://snurran.sics.se/hops/hopsbench.tgz"
+default[:hopsbench][:jar_url] = "http://snurran.sics.se/hops/hopsbench-apache.tgz"
 default[:hopsbench][:version] = "0.1"
 default[:hopsbench][:user] = node[:hdfs][:user]
 default[:hopsbench][:group] = node[:hadoop][:group]
